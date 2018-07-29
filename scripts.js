912bcee1e381a7281b0492553cc222c5f3febe12
Paul Boeck <boeck87@gmail.com>
2018-07-29 12:14:06 +0200

    Add basic upload to S3

--- scripts.js
+++ scripts.js
@@ -4,49 +4,31 @@ pages[0].style.display = 'block';
 
 const inputPhoto = document.getElementById('input-photo');
 
+AWS.config.update({
+    region: 'eu-central-1',
+    credentials: new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: 'eu-central-1:dde322a9-fc9b-4290-b627-6193ee4de47d',
+    })
+})
+
+var s3 = new AWS.S3({
+    apiVersion: '2006-03-01',
+    params: { Bucket: 'memory-wall' }
+});
+
 function onPhotoInputChange(e) {
 
-    const options = {
-        contain: true,
-        orientation: true,
-        canvas: true,
-        pixelRatio: devicePixelRatio
-    };
-
-    function onImageLoad(result) {
-        if (result.type === 'error') {
-            console.error('Error loading image', result);
+    s3.upload({
+        Key: 'photos/' + e.target.files[0].name,
+        Body: e.target.files[0],
+        ACL: 'public-read'
+    }, (err, data) => {
+        if (err) {
+            console.error(err)
         } else {
-
-            console.log('Generated canvas width and height', result.width, result.height);
-
-            // Replace our default canvas (for video) with the generated one
-            result.id = 'canvas-camera';
-
-            annotateCameraContainer.removeChild(cameraCanvas);
-            annotateCameraContainer.appendChild(result);
-
-            cameraCanvas = result;
-
-            const newWidth = cameraCanvas.style.width ? parseInt(cameraCanvas.style.width) : cameraCanvas.width;
-            const newHeight = cameraCanvas.style.height ? parseInt(cameraCanvas.style.height) : cameraCanvas.height;
-
-            // Make drawing canvas the same size
-            drawCanvas.width = newWidth;
-            drawCanvas.height = newHeight;
-            emojiCanvas.width = newWidth;
-            emojiCanvas.height = newHeight;
-
-            setLiveCamera(false);
-
-            AnnotatePage.show();
-        }
-    }
-
-    // A little library which handles rotating the image appropriately depending
-    // on the image's orientation (determined from the exif data) & scaling to fit
-    LoadImage(e.target.files[0], onImageLoad, options);
-
+            console.info('Successfully uploaded photo.');
+        } 
+    });
 }
 
 inputPhoto.addEventListener('change', onPhotoInputChange);
