bd9ae1df1ab8312ea42e822606048217ac26c482
Paul Boeck <boeck87@gmail.com>
2018-07-29 16:49:19 +0200

    Basic styles

--- scripts.js
+++ scripts.js
@@ -37,7 +37,7 @@ function onPhotoInputChange(e) {
 }
 
 function showImages() {
-    s3.listObjects({ Prefix: albumKey }, function (err, data) {
+    s3.listObjects({ Prefix: albumKey}, function (err, data) {
         if (err) {
             console.error(err);
             return;
@@ -45,16 +45,20 @@ function showImages() {
         // `this` references the AWS.Response instance that represents the response
         const href = this.request.httpRequest.endpoint.href;
         const bucketUrl = href + bucketName + '/';
-        const photoHtml = data.Contents.map(c => {
-            const url = bucketUrl + encodeURIComponent(c.Key);
-            console.log(url)
-            return `
+        const photoHtml = data.Contents
+            .filter(c => c.Size) // filter directories
+            .sort((a,b) => b.LastModified.valueOf() - a.LastModified.valueOf())
+            .slice(0,4)
+            .map(c => {
+                console.info(c)
+                const url = bucketUrl + encodeURIComponent(c.Key);
+                return `
                 <div class="polaroid">
-                    <p>Sarah, Dec '02</p>
+                    <p>Sarah, ${c.LastModified}</p>
                     <img src="${url}" />
                 </div>
             `
-        })
+            })
             .join('');
         pages[1].style.display = 'block';
         imageContainer.innerHTML = photoHtml;
