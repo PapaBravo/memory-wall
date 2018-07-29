918cf3a4b7e855a47e85f7294d669424c79921bf
Paul Boeck <boeck87@gmail.com>
2018-07-29 14:29:45 +0200

    Add read/write for S3

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
 const pages = document.getElementsByClassName('page');
 
+const albumKey = 'photos/';
+const bucketName = 'memory-wall';
+
+
 pages[0].style.display = 'block';
 
 const inputPhoto = document.getElementById('input-photo');
+const imageContainer = document.getElementById('image-container');
 
 AWS.config.update({
     region: 'eu-central-1',
@@ -13,13 +18,13 @@ AWS.config.update({
 
 var s3 = new AWS.S3({
     apiVersion: '2006-03-01',
-    params: { Bucket: 'memory-wall' }
+    params: { Bucket: bucketName }
 });
 
 function onPhotoInputChange(e) {
 
     s3.upload({
-        Key: 'photos/' + e.target.files[0].name,
+        Key: albumKey + e.target.files[0].name,
         Body: e.target.files[0],
         ACL: 'public-read'
     }, (err, data) => {
@@ -27,8 +32,34 @@ function onPhotoInputChange(e) {
             console.error(err)
         } else {
             console.info('Successfully uploaded photo.');
-        } 
+        }
     });
 }
 
+function showImages() {
+    s3.listObjects({ Prefix: albumKey }, function (err, data) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        // `this` references the AWS.Response instance that represents the response
+        const href = this.request.httpRequest.endpoint.href;
+        const bucketUrl = href + bucketName + '/';
+        const photoHtml = data.Contents.map(c => {
+            const url = bucketUrl + encodeURIComponent(c.Key);
+            console.log(url)
+            return `
+                <div class="polaroid">
+                    <p>Sarah, Dec '02</p>
+                    <img src="${url}" />
+                </div>
+            `
+        })
+            .join('');
+        pages[1].style.display = 'block';
+        imageContainer.innerHTML = photoHtml;
+    })
+}
+
 inputPhoto.addEventListener('change', onPhotoInputChange);
+showImages();
